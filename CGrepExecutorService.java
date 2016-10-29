3a91895bd48cc257e6b2f77a5c75535536607874
mderb4 <mderb4@gmail.com>
2016-10-29 19:08:35 -0400

    File Skeletons added

---
+++ CGrepExecutorService.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorCompletionService;
+
+public class CGrepExecutorService extends ExecutorCompletionService<Object>{
+
+	public CGrepExecutorService(Executor arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
