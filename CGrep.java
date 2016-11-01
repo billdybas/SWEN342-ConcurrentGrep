382bad4f3a7467df0c3b3cc0a23189d110730eaa
mderb4 <mderb4@gmail.com>
2016-10-31 21:18:09 -0400

    Removed not needed files, added executor to CGrep.

--- CGrep.java
+++ CGrep.java
@@ -1,8 +1,15 @@
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CGrep {
 
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ExecutorService executor = Executors.newFixedThreadPool(3);
+		CompletionService<Found> completionService = new ExecutorCompletionService<Found>(executor);
+		
 
 	}
 
